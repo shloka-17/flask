14daaa17682cdcef0ebb1fccbbcad1e47c2d6a46
AsiyaRahman13 <145169359+AsiyaRahman13@users.noreply.github.com>
2023-10-06 18:52:25 -0500

    hhh

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return '<h1> HI THERE</h1>'
+
+@app.route('/info')
+def info():
     return 'I am in 9th grade'
 
 if __name__ == "__main__":
     app.run(debug=True)
+    '<h1> Home Page'
